df9eb9c5144b0b1bfca9257832392cd2cc806e30
sebhajek <seb.hajek@gmail.com>
2024-01-31 20:42:46 +0100

    fixed fonts location

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo zypper -n in grim slurp;
 
-mkdir -p ~/setup-dots/fonts;
-cd ~/setup-dots/fonts;
+mkdir -p ~/.setup-dots/fonts;
+cd ~/.setup-dots/fonts;
 wget https://github.com/IBM/plex/releases/download/v6.4.0/OpenType.zip -O ibm-plex.zip;
-cd ~/setup-dots;
+cd ~/.setup-dots;
