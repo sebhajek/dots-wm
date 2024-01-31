b3187a2ef6592852f1e7cd8786d3174008166b77
sebhajek <seb.hajek@gmail.com>
2024-01-31 20:33:11 +0100

    fixed repo info of ibmplex fonts

--- setup.sh
+++ setup.sh
@@ -3,5 +3,4 @@
 sudo zypper -n in grim slurp;
 
 mkdir -p ~/setup-dots/fonts;
-
-gh release download --pattern 'OpenType.zip' --dir ~/setup-dots/fonts -r IBM/plex;
+gh release download --pattern 'OpenType.zip' --dir ~/setup-dots/fonts -R IBM/plex;
