c58e377ccd4f7b73650389df07e27e08c6be73d9
sebhajek <seb.hajek@gmail.com>
2024-01-31 20:40:14 +0100

    changed the font from gh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,6 @@
 sudo zypper -n in grim slurp;
 
 mkdir -p ~/setup-dots/fonts;
-gh release download --pattern 'OpenType.zip' --dir ~/setup-dots/fonts -R IBM/plex;
+cd ~/setup-dots/fonts;
+wget https://github.com/IBM/plex/releases/download/v6.4.0/OpenType.zip -O ibm-plex.zip;
+cd ~/setup-dots;
