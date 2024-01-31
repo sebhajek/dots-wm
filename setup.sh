35ff1e8b365e634558af3747f2e0c5e34071df00
sebhajek <seb.hajek@gmail.com>
2024-01-31 20:53:36 +0100

    added font extraction

--- setup.sh
+++ setup.sh
@@ -2,14 +2,14 @@
 
 sudo zypper -n in grim slurp;
 
-mkdir -p ~/.setup-dots/fonts/bitmap;
+mkdir -p ~/.setup-dots/fonts/out/bitmap;
 cd ~/.setup-dots/fonts;
 wget https://github.com/IBM/plex/releases/download/v6.4.0/OpenType.zip -O ibm-plex.zip;
-wget https://github.com/molarmanful/eldur/releases/download/v0.0.2/eldur.otb -O bitmap/eldur.otp;
-wget https://github.com/slavfox/Cozette/releases/download/v.1.23.2/cozette.otb -O bitmap/cozette.otp;
-wget https://github.com/slavfox/Cozette/releases/download/v.1.23.2/cozette_hidpi.otb -O bitmap/cozette-hidpi.otp;
+wget https://github.com/molarmanful/eldur/releases/download/v0.0.2/eldur.otb -O out/bitmap/eldur.otp;
+wget https://github.com/slavfox/Cozette/releases/download/v.1.23.2/cozette.otb -O out/bitmap/cozette.otp;
+wget https://github.com/slavfox/Cozette/releases/download/v.1.23.2/cozette_hidpi.otb -O out/bitmap/cozette-hidpi.otp;
 
-mkdir -p ~/.setup-dots/fonts/ibm-plex;
-cd ~/.setup-dots/fonts/ibm-plex;
+mkdir -p ~/.setup-dots/fonts/out/ibm-plex;
+cd ~/.setup-dots/fonts/out/ibm-plex;
 7z x ~/.setup-dots/fonts/ibm-plex.zip;
 cd ~/.setup-dots;
