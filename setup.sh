6443f7f3c57ce3c8276fbfaada9dec1feae5e274
sebhajek <seb.hajek@gmail.com>
2024-02-04 00:55:18 +0100

    greetd

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo zypper -n in -tpackage --no-recommends sway-branding-upstream sway swayidle swaylock swaybg waybar dunst rofi; #swaybar
-sudo zypper -n in -tpackage --no-recommends greetd gtkgreet;
+sudo zypper -n in -tpackage --no-recommends greetd gtkgreet cage;
 sudo zypper -n in grim slurp;
 
 mkdir -p ~/.config/sway;
@@ -10,3 +10,10 @@ mkdir -p ~/.config/waybar;
 cp -ur ~/.setup-dots/dots/wm/waybar/* ~/.config/waybar;
 mkdir -p ~/.config/dunst;
 cp -ur ~/.setup-dots/dots/wm/dunst/* ~/.config/dunst;
+mkdir -p /etc/sway/config.d;
+mkdir -p /etc/greetd/;
+cp -ur ~/.setup-dots/dots/wm/greetd/* /etc/greetd/;
+
+sudo useradd -M -G video greeter;
+sudo chmod -R go+r /etc/greetd/;
+systemctl enable --now greetd;
