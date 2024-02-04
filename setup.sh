ad27fae79b9250633ec4749126dc86e05ef3a387
sebhajek <seb.hajek@gmail.com>
2024-02-04 08:07:18 +0100

    sddm

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 sudo zypper -n in -tpackage --no-recommends sway-branding-upstream sway swayidle swaylock swaybg waybar dunst rofi; #swaybar
-sudo zypper -n in -tpackage --no-recommends greetd gtkgreet cage;
+#sudo zypper -n in -tpackage --no-recommends greetd gtkgreet cage;
+sudo zypper -n in -tpackage --no-recommends sddm;
 sudo zypper -n in grim slurp;
 
 mkdir -p ~/.config/sway;
@@ -12,8 +13,9 @@ mkdir -p ~/.config/dunst;
 cp -ur ~/.setup-dots/dots/wm/dunst/* ~/.config/dunst;
 mkdir -p /etc/sway/config.d;
 
-mkdir -p /etc/greetd/;
-sudo useradd -M -G video greeter;
-sudo chmod -R go+r /etc/greetd/;
-sudo systemctl enable greetd;
-sudo cp -ur ~/.setup-dots/dots/wm/greetd/* /etc/greetd/;
+#mkdir -p /etc/greetd/;
+#sudo useradd -M -G video greeter;
+#sudo chmod -R go+r /etc/greetd/;
+#sudo systemctl enable greetd;
+#sudo cp -ur ~/.setup-dots/dots/wm/greetd/* /etc/greetd/;
+sudo update-alternatives --set default-displaymanager /usr/lib/X11/displaymanagers/sddm;
