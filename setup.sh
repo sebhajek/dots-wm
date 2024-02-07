35cd193d1c2de66e4341ce6725677c0c09e8d44b
sebhajek <seb.hajek@gmail.com>
2024-02-07 08:54:33 +0100

    sddm

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo zypper -n in -tpackage --no-recommends sway-branding-upstream sway swayidle swaylock swaybg waybar dunst rofi; #swaybar
 sudo zypper -n in xorg-x11 xorg-x11-server;
-sudo zypper -n in -tpackage --no-recommends lightdm lightdm-gtk-greeter-branding-upstream;
+sudo zypper -n in -tpackage --no-recommends sddm sddm-branding-upstream;
 sudo zypper -n in grim slurp;
 
 mkdir -p ~/.config/sway;
@@ -12,7 +12,7 @@ cp -ur ~/.setup-dots/dots/wm/waybar/* ~/.config/waybar;
 mkdir -p ~/.config/dunst;
 cp -ur ~/.setup-dots/dots/wm/dunst/* ~/.config/dunst;
 
-sudo cp -ur ~/.setup-dots/dots/wm/lightdm/* /etc/lightdm/
-sudo update-alternatives --set default-displaymanager /usr/lib/X11/displaymanagers/lightdm;
+#sudo cp -ur ~/.setup-dots/dots/wm/lightdm/* /etc/lightdm/
+sudo update-alternatives --set default-displaymanager /usr/lib/X11/displaymanagers/sddm;
 sudo systemctl set-default graphical.target;
 sudo systemctl enable display-manager;
